7f81e7ba01486a67119313239fa7aab3fd246f45
Erik Scow <erikscow@gmail.com>
2020-01-24 19:09:16 -0800

    Completed tabs section using axios librry and .then method

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,20 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+//retrieve data
+const tabsData = axios.get('https://lambda-times-backend.herokuapp.com/topics')
+console.log(tabsData)
+
+//select the entry point div
+const topicsDiv = document.querySelector('.topics')
+
+//get into the arrray and for each item in it create and append the tab
+tabsData.then(response => {
+    response.data.topics.forEach(item => {
+        const newTab = document.createElement('div')
+        newTab.classList.add('tab')
+        newTab.textContent = `${item}`
+        topicsDiv.appendChild(newTab)
+    })
+})
